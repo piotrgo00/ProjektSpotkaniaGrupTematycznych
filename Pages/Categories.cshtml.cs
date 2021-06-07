d209d97387587283d97c60409a4ae61364c4ef4a
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2021-06-07 13:13:45 +0200

    Category - basic

---
+++ Pages/Categories.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjektSpotkaniaGrupTematycznych.Data;
+using ProjektSpotkaniaGrupTematycznych.Models;
+
+namespace ProjektSpotkaniaGrupTematycznych.Pages
+{
+    public class CategoriesModel : PageModel
+    {
+        private readonly ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext _context;
+
+        public CategoriesModel(ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
---
+++ Pages/CreateCategory.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjektSpotkaniaGrupTematycznych.Data;
+using ProjektSpotkaniaGrupTematycznych.Models;
+
+namespace ProjektSpotkaniaGrupTematycznych.Pages
+{
+    public class CreateCategoryModel : PageModel
+    {
+        private readonly ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext _context;
+
+        public CreateCategoryModel(ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Category.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Categories");
+        }
+    }
+}

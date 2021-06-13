fce9ebaed57bbca7a0c294c8531baca8d12ede50
Avitose <kierowcataczkipl@gmail.com>
2021-06-13 19:42:29 +0200

    Update Meetings

--- Pages/Meet/Meetings.cshtml.cs
+++ Pages/Meet/Meetings.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,20 +12,31 @@ using ProjektSpotkaniaGrupTematycznych.Models;
 
 namespace ProjektSpotkaniaGrupTematycznych.Pages.Meet
 {
+    [Authorize]
     public class MeetingsModel : PageModel
     {
-        private readonly ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext _context;
 
-        public MeetingsModel(ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public MeetingsModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
-        public IList<Models.Meeting> Meeting { get;set; }
+        public IList<Meeting> Meetings { get;set; }
 
         public async Task OnGetAsync()
         {
-            Meeting = await _context.Meeting.ToListAsync();
+            var uid = _userManager.GetUserId(User);
+            List<UserGroup> userGroups = _context.UserGroups.Where(e => e.UserId == uid).ToList();
+            List<int> userGroupsIds = new List<int>();
+            foreach(var item in userGroups)
+            {
+                userGroupsIds.Add(item.GroupId);
+            }
+            //group = await _context.Group.Include(g => g.Members).ThenInclude(y => y.User).Include(group => group.Meetings).FirstOrDefaultAsync(m => m.Id == gid);
+            Meetings = _context.Meeting.Where(e => userGroupsIds.Contains(e.GroupID)).ToList();
         }
     }
 }

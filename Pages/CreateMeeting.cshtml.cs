7a3fccffbe46d06aa24f8c33f82458ce434f62bf
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2021-06-13 17:04:50 +0200

    CreateMeeting

---
+++ Pages/CreateMeeting.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjektSpotkaniaGrupTematycznych.Data;
+using ProjektSpotkaniaGrupTematycznych.Models;
+
+namespace ProjektSpotkaniaGrupTematycznych.Pages
+{
+    public class CreateMeetingModel : PageModel
+    {
+        private readonly ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext _context;
+
+        public CreateMeetingModel(ProjektSpotkaniaGrupTematycznych.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public int _id { get; set; }
+        public IActionResult OnGetAsync(int id)
+        {
+            _id = id;
+            return Page();
+        }
+
+        [BindProperty]
+        public Meeting Meeting { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Meeting.GroupId = _id;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Meeting.Add(Meeting);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
